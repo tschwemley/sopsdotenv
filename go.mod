86ad631e98e0e9c888194446ad8667f62be0e603
Tyler Schwemley <tjschwem@gmail.com>
2024-04-28 22:38:38 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tschwemley/sopsdotenv
+
+go 1.22.2
---
+++ pkg/sopsdotenv.go
@@ -0,0 +1,17 @@
+package sopsdotenv
+
+import(
+	"github.com/getsops/sops/v3/decrypt"
+	"github.com/joho/godotenv"
+)
+
+type Env map[string]string
+
+func LoadFile(path string) (Env, error) {
+	envBytes, err := decrypt.File(path)
+	if err != nil {
+		panic(err)
+	}
+	return godotenv.UnmarshalBytes(envBytes)
+}
+
